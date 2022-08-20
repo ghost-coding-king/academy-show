c18dc4c21a937c5b6f25db80ad3fd6441c0144c1
WooKyungjin <wkj1103@naver.com>
2022-08-20 22:04:29 +0900

    경진: 불필요한 annotation 삭제

--- src/main/java/project/academyshow/repository/MemberRepository.java
+++ src/main/java/project/academyshow/repository/MemberRepository.java
@@ -1,12 +1,10 @@
 package project.academyshow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import project.academyshow.entity.Member;
 
 import java.util.Optional;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUsername(String username);
