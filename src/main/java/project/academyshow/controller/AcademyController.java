c0bbebb72c77a71e77357a099709c8ff622879db
WooKyungjin <wkj1103@naver.com>
2022-08-26 21:37:43 +0900

    학원 검색 response view 적용

--- src/main/java/project/academyshow/controller/AcademyController.java
+++ src/main/java/project/academyshow/controller/AcademyController.java
@@ -1,12 +1,15 @@
 package project.academyshow.controller;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.academyshow.controller.request.SearchRequest;
 import project.academyshow.controller.response.ApiResponse;
+import project.academyshow.entity.Academy;
 import project.academyshow.service.AcademyService;
 
 @RestController
@@ -19,6 +22,25 @@ public class AcademyController {
     /** 학원 검색 (Pageable: page(페이지), size(페이지 당 개수), sort(정렬 기준 필드명과 정렬방법) */
     @GetMapping("/academies")
     public ApiResponse<?> academies(SearchRequest searchRequest, Pageable pageable) {
-        return ApiResponse.success(academyService.search(searchRequest, pageable));
+        Page<AcademySearchResponse> responseData = academyService.search(searchRequest, pageable)
+                .map(AcademySearchResponse::new);
+        return ApiResponse.success(responseData);
+    }
+
+    @Data
+    private static class AcademySearchResponse {
+        private Long id;
+        private String name;
+        private String introduce;
+        private String roadAddress;
+        private String subAddress;
+
+        private AcademySearchResponse(Academy academy) {
+            this.id = academy.getId();
+            this.name = academy.getName();
+            this.introduce = academy.getIntroduce();
+            this.roadAddress = academy.getRoadAddress();
+            this.subAddress = academy.getSubAddress();
+        }
     }
 }
