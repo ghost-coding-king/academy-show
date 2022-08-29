189445609968b2f5677d5b8c6c08561e35e558ec
Dongwon Kim <freshtuna@kakao.com>
2022-08-29 21:20:11 +0900

    Post 리파지토리, findall 구현

--- src/main/java/project/academyshow/repository/PostRepository.java
+++ src/main/java/project/academyshow/repository/PostRepository.java
@@ -1,7 +1,24 @@
 package project.academyshow.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import project.academyshow.controller.request.PostRequest;
 import project.academyshow.entity.Post;
+import project.academyshow.entity.Review;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query(
+            value = "select p " +
+                    "from Post p " +
+                    "inner join fetch p.member " +
+                    "inner join fetch p.academy " +
+                    "where p.category = #{postRequest.category}",
+
+            countQuery = "select p " +
+                         "from Post p " +
+                         "where p.category = #{postRequest.category}"
+    )
+    Page<Review> findAll(PostRequest postRequest, Pageable pageable);
 }
