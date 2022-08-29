1f82fa4944488984b4691f89e168a64509069380
Dongwon Kim <freshtuna@kakao.com>
2022-08-29 16:06:22 +0900

    삭제 성공 응답 추가

--- src/main/java/project/academyshow/controller/response/ApiResponse.java
+++ src/main/java/project/academyshow/controller/response/ApiResponse.java
@@ -11,6 +11,9 @@ public class ApiResponse<T> {
     private String msg;
     private T data;
 
+    public static final ApiResponse DELETE_SUCCESS_RESPONSE =
+            new ApiResponse(200, "삭제에 성공했습니다", null);
+
     public ApiResponse(int code, String msg, T data) {
         if (data != null)
             this.count = data instanceof List ? ((List<?>) data).size() : 1;
