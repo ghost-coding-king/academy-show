297939493f704180e36b9ccb25e9f37109399448
WooKyungjin <wkj1103@naver.com>
2022-09-05 20:11:10 +0900

    경진: 학원 이름으로 검색 api 업데이트

--- src/main/java/project/academyshow/service/AcademyService.java
+++ src/main/java/project/academyshow/service/AcademyService.java
@@ -10,6 +10,7 @@ import project.academyshow.controller.response.ReviewStatistics;
 import project.academyshow.entity.Academy;
 import project.academyshow.entity.ReferenceType;
 
+import project.academyshow.entity.SearchType;
 import project.academyshow.repository.AcademyRepository;
 import project.academyshow.repository.ReviewRepository;
 
@@ -26,7 +27,10 @@ public class AcademyService {
     private final ReviewRepository reviewRepository;
 
     public Page<Academy> search(SearchRequest searchRequest, Pageable pageable) {
-        return academyRepository.findAll(searchRequest, pageable);
+        if (searchRequest.getSearchType() == SearchType.FILTER)
+            return academyRepository.findAll(searchRequest, pageable);
+        else
+            return academyRepository.findByNameContaining(searchRequest.getQ(), pageable);
     }
 
     public Optional<Academy> findById(Long id) {
