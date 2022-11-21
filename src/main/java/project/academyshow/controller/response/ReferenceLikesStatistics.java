03bf2aea6658572fd89f753dded64e4d50b64f29
Dongwon Kim <freshtuna@kakao.com>
2022-11-21 20:17:15 +0900

    ReferenceLikesStatistics 으로 이름변경

---
+++ src/main/java/project/academyshow/controller/response/ReferenceLikesStatistics.java
@@ -0,0 +1,29 @@
+package project.academyshow.controller.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ReferenceLikesStatistics {
+
+    private enum STATUS  {
+        NOT_USE, NOT_LOGIN, YES, NO
+    }
+
+    private Long count;
+    private STATUS status;    /** 좋아요 상태 */
+
+    public static ReferenceLikesStatistics of(long count, boolean isLike) {
+        return new ReferenceLikesStatistics(count, isLike ? STATUS.YES:STATUS.NO);
+    }
+
+    public static ReferenceLikesStatistics notAuthenticatedOf(long count) {
+        return new ReferenceLikesStatistics(count, STATUS.NOT_LOGIN);
+    }
+
+    public static ReferenceLikesStatistics justCountOf(long count) {
+        return new ReferenceLikesStatistics(count, STATUS.NOT_USE);
+    }
+}
